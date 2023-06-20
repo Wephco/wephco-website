aa29b3375f41d2a3d97c52c2d110a781380114a5
Neto <ukpongneto@gmail.com>
2023-06-20 09:55:24 +0100

    property listing page in progress

--- src/components/pages/PropertyListing/PropertyListing.tsx
+++ src/components/pages/PropertyListing/PropertyListing.tsx
@@ -2,23 +2,12 @@ import { useState, useCallback, useEffect, useContext } from 'react';
 import ApiHelper from '../../../utils/apiHelper';
 import NoData from '../../common/NoData';
 import { endpoints } from '../../../utils/URL';
-import { FaEdit, FaTrash } from 'react-icons/fa';
+import { Properties } from '../../../interfaces/PropertyListingInterface';
 import Loader from '../../common/Loader';
 import useAlertModal from '../../../hooks/useAlertModal';
 import { AppContext, AppContextType } from '../../../context/AppContext';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
-interface PropertyListingData {
-	location: string;
-	propertyType: string;
-	propertyImages: string[];
-	numberOfrooms: number;
-	numberOfToilets: number;
-	numberOfBathrooms: number;
-	numberOfLivingRooms: number;
-	numberOfKitchens: number;
-	agentId: number;
-}
 
 const PropertyListing = () => {
 	const { token } = useContext(AppContext) as AppContextType;
@@ -27,15 +16,12 @@ const PropertyListing = () => {
 
 	const api = new ApiHelper()
 
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	const [properties, setProperties] = useState<PropertyListingData[]>([]);
+	const [properties, setProperties] = useState<Properties[]>([]);
 	const [loading, setLoading] = useState(false);
 
-	const addProperty = () => {
-		navigate('/property-listings/add');
-	};
-
+	
 	const getProperties = useCallback(async () => {
 		setLoading(true);
 
@@ -43,7 +29,7 @@ const PropertyListing = () => {
 			const response = await api.getData(endpoints.PropertyListings.mainUrl, token);
 			setProperties(response.data);
 		} catch (error) {
-			alert.setContent('Error getting property listings')
+			alert.setContent('Error getting property listings. Try again later')
 			alert.setVariant('error')
 			alert.open()
 			
@@ -56,57 +42,11 @@ const PropertyListing = () => {
 		getProperties();
 	}, [getProperties]);
 
-	let tableHead = (
-		<thead>
-			<tr>
-				<th>Actions</th>
-				<th>Agent ID</th>
-				<th>Location</th>
-				<th>Property Type</th>
-				<th>Number of Rooms</th>
-				<th>Number of Toilets</th>
-				<th>Number of Bathrooms</th>
-				<th>Number of Living Rooms</th>
-				<th>Number of Kitchens</th>
-			</tr>
-		</thead>
-	);
-
-	let tableBody = (
-		<tbody>
-			{properties.map((property, index) => (
-				<tr key={index}>
-					<td className='flex flex-wrap'>
-						<div className='tooltip' data-tip='Edit Property'>
-							<FaEdit className='text-yellow-500 mr-2 cursor-pointer' />
-						</div>
-						<div className='tooltip' data-tip='Delete Property'>
-							<FaTrash className='text-red-500 cursor-pointer' />
-						</div>
-					</td>
-					<td>{property.agentId}</td>
-					<td>{property.location}</td>
-					<td>{property.propertyType}</td>
-					<td>{property.numberOfrooms}</td>
-					<td>{property.numberOfToilets}</td>
-					<td>{property.numberOfBathrooms}</td>
-					<td>{property.numberOfLivingRooms}</td>
-					<td>{property.numberOfKitchens}</td>
-				</tr>
-			))}
-		</tbody>
-	);
-
+	
 	return (
 		<div className=''>
-			<div className='flex justify-between items-center'>
-				<h1 className='text-2xl font-semibold'>Property Listings</h1>
-				<button
-					className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded shadow'
-					onClick={addProperty}
-				>
-					Add Property
-				</button>
+			<div className='items-center'>
+				<h1 className='text-2xl font-semibold my-16'>Local Property Listings</h1>
 			</div>
 
 			{loading && (
@@ -121,14 +61,7 @@ const PropertyListing = () => {
 				</div>
 			)}
 
-			{!loading && properties.length > 0 && (
-				<div className='w-full overflow-x-hidden'>
-					<table className='w-full table-auto border-collapse'>
-						{tableHead}
-						{tableBody}
-					</table>
-				</div>
-			)}
+			
 		</div>
 	);
 };
