387f8a4ee6014d5c61212935193c63cc96971fe2
Neto <ukpongneto@gmail.com>
2023-07-04 18:20:39 +0100

    add property form in progress

--- src/components/pages/PropertyListing/AddPropertyForm.tsx
+++ src/components/pages/PropertyListing/AddPropertyForm.tsx
@@ -1,37 +1,36 @@
 import { useState, useContext } from 'react';
 import { endpoints } from '../../../utils/URL';
 import ApiHelper from '../../../utils/apiHelper';
-import { CreateProperty, newProperty } from '../../../interfaces/PropertyListingInterface';
+import { newProperty } from '../../../interfaces/PropertyListingInterface';
 import { AppContext, AppContextType } from '../../../context/AppContext';
 
 const AddPropertyForm = () => {
-	const { token } = useContext(AppContext) as AppContextType
+	const { token } = useContext(AppContext) as AppContextType;
 
-	const [property, setProperty] = useState(newProperty)
-	const [loading, setLoading] = useState(false)
+	const [property, setProperty] = useState(newProperty);
+	const [loading, setLoading] = useState(false);
 
-	const api = new ApiHelper()
+	const api = new ApiHelper();
 
 	const handleChange = (input: string) => (e: any) => {
 		setProperty({
 			...property,
-			[input]: e.target.value
-		})
-	}
+			[input]: e.target.value,
+		});
+	};
 
 	const submitProperty = async (e: any) => {
-		e.preventDefault()
+		e.preventDefault();
 
-		setLoading(true)
+		setLoading(true);
 
 		try {
-			await api.postData(endpoints.PropertyListings.mainUrl, property, token)
+			await api.postData(endpoints.PropertyListings.mainUrl, property, token);
 		} catch (error) {
-			
 		} finally {
-			setLoading(false)
+			setLoading(false);
 		}
-	}
+	};
 
 	return (
 		<div className='flex flex-col justify-end items-start mt-10'>
@@ -45,7 +44,12 @@ const AddPropertyForm = () => {
 					</div>
 					<div className='form-group'>
 						<label htmlFor='location'>Location</label>
-						<input type='text' className='form-control' id='location' />
+						<input
+							onChange={handleChange('location')}
+							type='text'
+							className='form-control'
+							id='location'
+						/>
 					</div>
 					<div className='form-group'>
 						<label htmlFor='propertyType'>Property Type</label>
@@ -71,7 +75,7 @@ const AddPropertyForm = () => {
 						<label htmlFor='numberOfKitchens'>Number of Kitchens</label>
 						<input type='text' className='form-control' id='numberOfKitchens' />
 					</div>
-					<button type='submit' className='btn btn-warning'>
+					<button onClick={submitProperty} type='submit' className='btn btn-warning'>
 						Submit
 					</button>
 				</fieldset>
